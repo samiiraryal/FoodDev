875a9f334e14b695f0eae71dd0750aedcfc99a73
samiiraryal <95860466+samiiraryal@users.noreply.github.com>
2022-01-05 13:26:33 +0545

    Add files for FoodDev

---
+++ js/animate.js
@@ -0,0 +1,31 @@
+(function ($) {
+    $(document).ready(function () {
+		
+		
+
+        // Animations
+        //-----------------------------------------------
+        if (($("[data-animation-effect]").length > 0) && !Modernizr.touch) {
+            $("[data-animation-effect]").each(function () {
+                var $this = $(this),
+                    animationEffect = $this.attr("data-animation-effect");
+                if (Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+                    $this.appear(function () {
+                        var delay = ($this.attr("data-effect-delay") ? $this.attr("data-effect-delay") : 1);
+                        if (delay > 1) $this.css("effect-delay", delay + "ms");
+                        setTimeout(function () {
+                            $this.addClass('animated object-visible ' + animationEffect);
+                        }, delay);
+                    }, { accX: 0, accY: -130 });
+                } else {
+                    $this.addClass('object-visible');
+                }
+            });
+        };
+
+       
+ }); // End document ready
+
+})(this.jQuery);
+
+
